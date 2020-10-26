fd4d83e6b09c9cc96de8efd88b171e1bbeab4d4b
Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>
2020-10-26 16:35:31 -0400

    [ADJUST] Adjust the task
    Signed-off-by: Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,21 +39,31 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productsSubtotal = product.price * product.quantity;
 
-    return formatValue(0);
+      return accumulator + productsSubtotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productsQuantity = product.quantity;
+
+      return accumulator + productsQuantity;
+    }, 0);
+
+    return total;
 
     return 0;
   }, [products]);
@@ -92,13 +102,21 @@ const Cart: React.FC = () => {
                   testID={`increment-${item.id}`}
                   onPress={() => handleIncrement(item.id)}
                 >
-                  <FeatherIcon name="plus" color="#E83F5B" size={16} />
+                  <FeatherIcon
+                    name="plus"
+                    color="rgba(16, 172, 132,1.0)"
+                    size={16}
+                  />
                 </ActionButton>
                 <ActionButton
                   testID={`decrement-${item.id}`}
                   onPress={() => handleDecrement(item.id)}
                 >
-                  <FeatherIcon name="minus" color="#E83F5B" size={16} />
+                  <FeatherIcon
+                    name="minus"
+                    color="rgba(16, 172, 132,1.0)"
+                    size={16}
+                  />
                 </ActionButton>
               </ActionContainer>
             </Product>
